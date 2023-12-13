30e1f35598221be702423b52f545d5fab8433729
david leon <jdleonch@gmail.com>
2023-12-13 18:12:42 -0500

    infinite scroll

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,29 +1,67 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
 
-const RANDOM_FACT_URL = 'https://catfact.ninja/fact'
-const RANDOM_CAT_IMG_URL = 'https://cataas.com/cat/says/hello'
+const RANDOM_FACT_URL = 'https://catfact.ninja/fact';
 
-const fetchData = async url => {
-  const response = await fetch(url)
-  const data = await response.json()
-  return data
-}
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
 function App() {
-  const [fact, setFact] = useState(null)
+  const [facts, setFacts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const lastElementRef = useRef(null);
 
   useEffect(() => {
-    fetchData(RANDOM_FACT_URL).then(data => setFact(data.fact))
-  }, [])
+    fetchData(RANDOM_FACT_URL).then((data) => setFacts([{ text: data.fact, imageUrl: `https://cataas.com/cat/says/${data.fact}` }]));
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          loadMore();
+        }
+      },
+      { threshold: 1 }
+    );
+    if (lastElementRef.current) {
+      observer.observe(lastElementRef.current);
+    }
+    return () => observer.disconnect();
+  }, [loading]);
+
+  const loadMore = () => {
+    setLoading(true);
+
+    fetchData(RANDOM_FACT_URL)
+      .then((data) => {
+        setFacts((prevFacts) => [
+          ...prevFacts,
+          { text: data.fact, imageUrl: `https://cataas.com/cat/says/${data.fact}` },
+        ]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        setLoading(false);
+      });
+  };
 
   return (
     <div>
-      {fact ?? 'Cargando...'}
-      <br />
-      <img src={RANDOM_CAT_IMG_URL} alt="Random cat" />
+      {facts.map((fact, index) => (
+        <div key={index}>
+          {fact.text ?? 'Cargando...'}
+          <br />
+          <img src={fact.imageUrl} alt="Random cat" />
+        </div>
+      ))}
+      <div ref={lastElementRef} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
