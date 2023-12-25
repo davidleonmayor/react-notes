c408629c36c69a80e9012940d46ece6e1d9374d1
david leon <jdleonch@gmail.com>
2023-12-25 15:59:44 -0500

    add typeScript & feature mouse follower 03 repo

--- projects/03-useEffect-mouse-follower/src/App.jsx
+++ projects/03-useEffect-mouse-follower/src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useMousePosition  from "./hooks/useMousePosition";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { clientX, clientY } = useMousePosition();
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div 
+      style={{
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 9999,
+        pointerEvents: "none",
+        cursor: "none"
+      }}
+    >
+      <svg
+        width={200}
+        height={200}
+        viewBox="0 0 50 50"
+        style={{
+          position: "absolute",
+          left: clientX,
+          top: clientY,
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <circle
+          cx="25"
+          cy="25"
+          r="8"
+        />
+      </svg>
+    </div>
+  );
 }
 
 export default App
